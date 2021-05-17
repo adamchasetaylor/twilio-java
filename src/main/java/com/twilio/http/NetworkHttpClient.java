43d45ac1be94169407846bd837dd690b450882e4
Adam <adamchasetaylor@users.noreply.github.com>
2021-05-17 15:40:54 -0500

    Revert "added in Exponential Backoff Strategy with AIMDBackoffManager"

--- src/main/java/com/twilio/http/NetworkHttpClient.java
+++ src/main/java/com/twilio/http/NetworkHttpClient.java
@@ -12,7 +12,6 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.impl.client.AIMDBackoffManager;
 import org.apache.http.message.BasicHeader;
 
 import java.io.IOException;
@@ -70,8 +69,6 @@ public class NetworkHttpClient extends HttpClient {
             .setDefaultRequestConfig(config)
             .setDefaultHeaders(headers)
             .setRedirectStrategy(this.getRedirectStrategy())
-            .setBackoffManager(new AIMDBackoffManager(connectionManager))
-            .setConnectionBackoffStrategy(new TwilioBackoffStrategy())
             .build();
     }
 
